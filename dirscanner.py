a26492f12baa0aa92bc43ea70707d4f6117ef70a
Fırat Esatoğlu <60032785+firatesatoglu@users.noreply.github.com>
2020-12-04 22:13:00 +0300

    Update dirscanner.py

--- dirscanner.py
+++ dirscanner.py
@@ -1,18 +1,23 @@
+# -*- coding: UTF-8 -*-
+#!/usr/bin/python
 import socket
 import sys
 import requests
+import argparse
 
-url = input("Url'i Girin: ")
-url = url.replace(" ", "").split(":") # url'ı : işareti ile ayırır 
+parametre = argparse.ArgumentParser()
+parametre.add_argument('-u', help="Taramak istediğin URL'i ver!")
+parametre.add_argument('-w', help="Kelime listesinin(wordlist) yolunu ver!")
+parametre.parse_args()
+url = sys.argv[2].replace(" ", "").split(":") # url'ı : işareti ile ayırır 
 webAddr = url[1].split("/")
 
 zamanAsimi = 10 #saniye başına bağlantı sayısı (saniye başına dizin tarama)
 portNumarasi = 0
-
-if url[0] == "http": #http veya https protokolüne göre port numarası atanır
-    portNumarasi = 80
-elif url[0] == "https":
+if "https" in url[0]: #https veya http protokolüne göre port numarası atanır
     portNumarasi = 443
+elif "http" in url[0]:
+    portNumarasi = 80
 
 serverIP = socket.gethostbyname(webAddr[2]) #IP adresini çözümler
 print("Hedef IP=", serverIP)
@@ -23,25 +28,25 @@ try:
     serverDurum = baglan.connect_ex((serverIP, portNumarasi))
 
     if serverDurum == 0:
-        kelimeListesi = input("Dizin taramak için kelime listesinin dosya yolunu belirtiniz= ")
-        print("Tarama başlatılıyor...")
-        dosya = open(kelimeListesi, "r", encoding="ISO-8859-1")
-        dosyaIcerigi = dosya.readlines()
+        print(webAddr[2])
     else:
         print("Hedef host kapalı, tarama başlayamadı :(")
         sys.exit()
     baglan.close()
+
+    print("Tarama başlatılıyor...")
+    dosya = open(sys.argv[4], "r", encoding="ISO-8859-1")
+    dosyaIcerigi = dosya.readlines()
 
     for kelime in dosyaIcerigi:
         if portNumarasi == 443:
             dizin = "https://" + webAddr[2] +"/"+ kelime
         elif portNumarasi == 80:
             dizin = "http://" + webAddr[2] +"/"+ kelime
-
         istek = requests.get(dizin)
         responseCode = str(istek.status_code)
         if responseCode == "200":
-            print(responseCode+ " =", dizin)
+            print(responseCode+ "= ", dizin )
 
-except Exception as hata:
-    print("Hata:", hata)
+except Exception as Hata:
+    print("Hata", Hata)
